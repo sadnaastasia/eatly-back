5f80daadb52fbfa7393e1fbd389fbb75234b843d
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-12-14 21:34:43 +0300

    vercel

--- server.js
+++ server.js
@@ -26,10 +26,9 @@ db.sequelize
   .sync({ force: false })
   .then(() => {
     console.log('Database synchronized');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}.`);
-    });
   })
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
+
+export default app;
