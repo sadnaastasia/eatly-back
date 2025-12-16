907c32d4a4e7442f6cb7bbdce6c6a6f428988cd3
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-12-16 12:03:06 +0300

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,7 +4,7 @@ export default {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_DB,
+  DB: process.env.DB_NAME,
   PORT: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
   pool: {
