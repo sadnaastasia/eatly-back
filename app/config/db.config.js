8f240e762e3be31a001e84884e39cdcecdafa833
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-12-16 13:11:24 +0300

    dialect

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -6,7 +6,6 @@ export default {
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
   PORT: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,7 @@ import roleModel from './role.model.js';
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+  dialect: 'mysql',
   dialectModule: mysql2,
   pool: dbConfig.pool,
   port: dbConfig.PORT,
